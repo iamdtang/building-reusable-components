ffb349302c799af44c79e1471a41d82cf0476fca
David Tang <687449+iamdtang@users.noreply.github.com>
2023-06-23 09:47:48 -0700

    Lenny 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.css";
+import StarRating from "./StarRating";
+import { useState } from "react";
+
+export default function App() {
+  const [rating, setRating] = useState(3);
+
+  return (
+    <div>
+      <StarRating
+        rating={rating}
+        onStarClick={(i) => {
+          setRating(i);
+        }}
+      />
+    </div>
+  );
+}
